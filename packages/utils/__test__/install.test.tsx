900aa0b9e0e8d1d5cb1725399bf8a64be81a3b2d
qingkongyiheQAQ <309158977@qq.com>
2025-03-07 14:16:26 +0800

    test

--- packages/utils/__test__/install.test.tsx
+++ packages/utils/__test__/install.test.tsx
@@ -3,7 +3,6 @@ import { mount } from "@vue/test-utils";
 import { defineComponent, createApp } from "vue";
 
 import { withInstall } from "../install";
-import makeInstaller from '../../core/makeInstaller'
 
 const AppComp = defineComponent({
   setup() {
@@ -12,22 +11,22 @@ const AppComp = defineComponent({
 })
 
 const compA = withInstall(defineComponent({
-  name:'CompA',
+  name: 'CompA',
   setup() {
     return () => <div>CompA</div>;
   },
 }))
 
 const compB = withInstall(defineComponent({
-  name:'CompB',
+  name: 'CompB',
   setup() {
     return () => <div>CompB</div>;
   },
 }))
 
-describe('install',()=>{
-  it('withInstall should be worked',()=>{
-    const wrapper = mount(()=><div id="app"></div>)
+describe('install', () => {
+  it('withInstall should be worked', () => {
+    const wrapper = mount(() => <div id="app"></div>)
     const app = createApp(AppComp)
 
     app.use(compA).mount(wrapper.element)
@@ -36,31 +35,5 @@ describe('install',()=>{
     expect(compB.install).toBeDefined()
     expect(app._context.components['CompA']).toBeTruthy()
     expect(app._context.components['CompB']).toBeFalsy()
-  })
-  it('makehInstaller should be worked', () => {
-    const wrapper = mount(() => <div id="app"></div>)
-    const app = createApp(AppComp)
-    const installer = makeInstaller([compA, compB]);
-    app.use(installer).mount(wrapper.element)
-
-    expect(installer).toBeDefined()
-    expect(wrapper.findComponent(compA)).toBeTruthy()
-    expect(wrapper.findComponent(compB)).toBeTruthy()
-  })
-  it('withInstall should work with unnamed component', () => {
-    const wrapper = mount(() => <div id="app"></div>)
-    const app = createApp(AppComp)
-
-    // 创建一个无 name 组件
-    const unnamedComp = withInstall(defineComponent({
-      setup() {
-        return () => <div>Unnamed</div>;
-      },
-    }));
-
-    app.use(unnamedComp).mount(wrapper.element)
-
-    // Vue 应该把它注册为 "UnnamedComponent"
-    expect(app._context.components['UnnamedComponent']).toBeTruthy()
   })
 })
